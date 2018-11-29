ed00befeea6abc5ae9eaf7bf949fd57a7934399a
ralambotianamio <miora.ralambotiana@gmail.com>
2018-11-29 11:03:53 +0100

    add csv-export + main class to test

---
+++ csv-exporter/src/main/java/powsybl/tutorials/csv/export/Main.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2018, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package powsybl.tutorials.csv.export;
+
+import com.powsybl.iidm.export.Exporters;
+import com.powsybl.iidm.network.Network;
+import com.powsybl.iidm.network.test.EurostagTutorialExample1Factory;
+
+import java.nio.file.Paths;
+
+/**
+ * @author Miora Ralambotiana <miora.ralambotiana at rte-france.com>
+ */
+public final class Main {
+
+    public static void main(String[] args) {
+        Network network = EurostagTutorialExample1Factory.create();
+        Exporters.export("CSV", network, null, Paths.get("/tmp/test.csv"));
+    }
+
+    private Main() {
+    }
+
+}

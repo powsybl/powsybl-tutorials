ddf9b153484aafb30b35d38e0bcdac97ca3e1400
ortega-herguetamar <maria.hergueta-ortega@rte-france.com>
2019-01-23 16:24:39 +0100

    Groovy file - resources of import post processor tutorial

---
+++ importer-post-processor/src/main/resources/increase-active-power-postprocessor.groovy
@@ -0,0 +1,36 @@
+
+package com.powsybl.samples.groovyScriptPostProcessor
+
+import com.powsybl.iidm.network.Load
+import com.powsybl.iidm.network.Network
+import com.powsybl.loadflow.LoadFlowFactory
+import com.powsybl.loadflow.LoadFlowParameters
+import com.powsybl.commons.config.ComponentDefaultConfig
+
+println " Imported Network's Data: Network Id: " + network.getId()  + "  Generators: " + network.getGeneratorCount()+ "  Lines : " + network.getLineCount() +" Loads: " + network.getLoadCount()
+
+println "\nDump LOADS "
+println " id | p | p+1%"
+
+// change the network
+def  percent = 1.01
+
+network.getLoads().each { load ->
+    if ( load.getTerminal != null) {
+        def currentValue = load.getTerminal().getP()
+        load.getTerminal().setP(currentValue * percent)
+        def newVal = load.getTerminal().getP()
+        println " " + load.getId() + "| " +currentValue + "| " + newVal
+    }
+}
+
+// execute a LF
+println "\nExecute a LF"
+
+def defaultConfig = ComponentDefaultConfig.load()
+loadFlowFactory = defaultConfig.newFactoryImpl(LoadFlowFactory.class)
+loadFlowParameters = new LoadFlowParameters(LoadFlowParameters.VoltageInitMode.UNIFORM_VALUES)
+loadFlow = loadFlowFactory.create(network, computationManager, 0)
+result = loadFlow.run(network.getStateManager().getWorkingStateId(),loadFlowParameters).join()
+
+println " LF results - converge:" + result.ok + " ; metrics: " +result.getMetrics()

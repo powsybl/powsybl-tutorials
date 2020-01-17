a39a1981c0ba4e137cc53c806a51c530b5d33d00
Agnès Leroy <agnes.leroy@rte-france.com>
2020-01-17 11:05:18 +0100

    Create a sensitivity tutorial
    It runs a sensitivity computation on a network composed of
    12 nodes, monitoring 4 lines with respect to a PST tap position.
    Then, it defines contingencies and performs a systematic sensitivity analysis.
    
    Signed-off-by: Agnès Leroy <agnes.leroy@rte-france.com>

---
+++ sensitivity/src/main/java/powsybl/tutorials/sensitivity/SensitivityTutorial.java
@@ -0,0 +1,194 @@
+package powsybl.tutorials.sensitivity;
+
+import com.powsybl.computation.ComputationManager;
+import com.powsybl.computation.local.LocalComputationManager;
+import com.powsybl.contingency.BranchContingency;
+import com.powsybl.contingency.ContingenciesProvider;
+import com.powsybl.contingency.Contingency;
+import com.powsybl.iidm.import_.Importers;
+import com.powsybl.iidm.network.Bus;
+import com.powsybl.iidm.network.Line;
+import com.powsybl.iidm.network.Network;
+import com.powsybl.iidm.network.VariantManagerConstants;
+import com.powsybl.iidm.xml.NetworkXml;
+import com.powsybl.loadflow.LoadFlow;
+import com.powsybl.loadflow.LoadFlowParameters;
+import com.powsybl.sensitivity.*;
+import com.powsybl.sensitivity.converter.JsonSensitivityComputationResultExporter;
+import com.powsybl.sensitivity.converter.SensitivityComputationResultExporter;
+import com.powsybl.sensitivity.factors.BranchFlowPerPSTAngle;
+import com.powsybl.sensitivity.factors.functions.BranchFlow;
+import com.powsybl.sensitivity.factors.variables.PhaseTapChangerAngle;
+import com.rte_france.powsybl.hades2.sensitivity.Hades2SensitivityFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @author Agnes Leroy <agnes.leroy at rte-france.com>
+ */
+public final class SensitivityTutorial {
+    private static final Logger LOGGER = LoggerFactory.getLogger(SensitivityTutorial.class);
+
+    public static void main(String[] args) throws IOException {
+
+        // 1. Import the network from a XML file
+        // The network is described in the iTesla Internal Data Model format.
+        File file = new File(SensitivityTutorial.class.getResource("/sensi_network_12_nodes.xml").getPath());
+        Network network = Importers.loadNetwork(file.toString());
+        // This network consists of 12 buses:
+        //     - we are considering schematic exchanges between France, Belgium, Germany and the Netherlands
+        //     - there are 3 buses per country
+        //     - there is one phase-shift transformer in Belgium
+        //     - there are 4 lines we would wish to monitor in this case:
+        //         - Belgium - France
+        //         - France - Germany
+        //         - Germany - the Netherlands
+        //         - the Netherlands - Belgium
+        //     - we are monitoring the effect of the phase-shift transformer or of the generators' injections on
+        //       each of these lines.
+        // In this tutorial the calculations are done in DC mode, but this can be changed in the config file
+        // by setting dcMode = false in the hades2-default-parameters.
+
+        // In order to compute a sensitivity analysis on this network,
+        // the sensitivity engine used should be defined in the local config file.
+        // Here we use the Hades2 sensitivity engine, which comes together with the
+        // loadflow. The configuration is thus the same as the loadflow.
+        // Check the loadflow tutorial for more information on the configuration.
+
+        // 2. Run an initial loadflow in order to output an iidm file with the P values
+        // This is an optional section: it is not necessary to launch the loadflow explicitly
+        // for the sensitivity study with the option computeInitialLoadflow: true.
+        LoadFlow.run(network, LoadFlowParameters.load());
+        // Write the IIDM network in xml format into a file in the results directory.
+        String resourcesPath = Paths.get(SensitivityTutorial.class.getResource("/").getPath(),
+                "../../src/main/resources").toString();
+        Path iidmPath = Paths.get(resourcesPath, "./network_after_loadflow.xml").toAbsolutePath();
+        File iidmFile = new File(iidmPath.toString());
+        // If the file doesn't exist, create it.
+        if (!iidmFile.exists()) {
+            iidmFile.createNewFile();
+        }
+        // Write the data into the file.
+        try (FileOutputStream os = new FileOutputStream(iidmFile.toString())) {
+            NetworkXml.write(network, os);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+        // Print the buses' active power in the terminal
+        for (Bus bus : network.getBusView().getBuses()) {
+            double activePower = bus.getP();
+            LOGGER.info("BUS {} - P: {}", bus.getId(), activePower);
+        }
+
+        // 3. Create a list of factors to be studied in the sensitivity computation
+        // There are four types of factors available, check the Powsybl website for more information.
+        // Here we study the effect of the Belgian phase shift transformer
+        // on the flow through each of the lines connecting the countries.
+        // We thus define these lines as monitoredBranch, and create one factor of type
+        // BranchFlowPerPSTAngle for each monitoredBranch.
+        // TODO: read the CNEC and the RA data from a CRAC file instead of hard-coding their definition?
+
+        // Start by defining the monitored lines.
+        List<Line> monitoredLines = new ArrayList<>();
+        monitoredLines.add(network.getLine("BBE2AA1  FFR3AA1  1"));
+        monitoredLines.add(network.getLine("FFR2AA1  DDE3AA1  1"));
+        monitoredLines.add(network.getLine("DDE2AA1  NNL3AA1  1"));
+        monitoredLines.add(network.getLine("NNL2AA1  BBE3AA1  1"));
+
+        // Then create the factors.
+        // TODO: refactor the API to simplify this part - use a builder pattern?
+        SensitivityFactorsProvider twtFactorsProvider = net -> {
+            List<SensitivityFactor> factors = new ArrayList<>();
+            monitoredLines.forEach(l -> {
+                String monitoredBranchId = l.getId();
+                String monitoredBranchName = l.getName();
+                BranchFlow branchFlow = new BranchFlow(monitoredBranchId, monitoredBranchName, l.getId());
+                String twtId = network.getTwoWindingsTransformer("BBE2AA1  BBE3AA1  1").getId();
+                factors.add(new BranchFlowPerPSTAngle(branchFlow,
+                        new PhaseTapChangerAngle(twtId, twtId, twtId)));
+            });
+            return factors;
+        };
+
+        // 4. Run the sensitivity analysis
+        // Create a ComputationManager in order to perform the computations.
+        // TODO: make some refactoring like in the loadflow to avoid explicitly creating the computation manager
+        ComputationManager computationManager = LocalComputationManager.getDefault();
+        // Create a SensitivityComputation object.
+        // TODO: refactor also to do it like the loadflow
+        SensitivityComputation sensitivityComputation = new Hades2SensitivityFactory().create(network,
+                computationManager, 1);
+        // Run the computation.
+        SensitivityComputationResults sensitivityTwtComputationResults = sensitivityComputation.run(twtFactorsProvider,
+                VariantManagerConstants.INITIAL_VARIANT_ID, SensitivityComputationParameters.load()).join();
+
+        // 5. Output the results
+        // Write the sensitivity results in a JSON file.
+        Path jsonTwtResultPath = Paths.get(resourcesPath, "./sensi_result.json").toAbsolutePath();
+        File jsonTwtResultFile = new File(jsonTwtResultPath.toString());
+        // If the file doesn't exist, create it
+        if (!jsonTwtResultFile.exists()) {
+            jsonTwtResultFile.createNewFile();
+        }
+        SensitivityComputationResultExporter exporter = new JsonSensitivityComputationResultExporter();
+        try (FileOutputStream os = new FileOutputStream(jsonTwtResultFile.toString())) {
+            exporter.export(sensitivityTwtComputationResults, new OutputStreamWriter(os));
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+
+        // 7. Perform a systematic sensitivity analysis
+        // TODO: implement the API for the Hades2 systematic sensitivity studies
+        // First, implement a contingencies provider.
+        // Here the list of contingencies is composed of the lines that are not monitored
+        // in the sensitivity analysis.
+        ContingenciesProvider contingenciesProvider = n -> n.getLineStream()
+                .filter(l -> {
+                    final boolean[] isContingency = {true};
+                    monitoredLines.forEach(monitoredLine -> {
+                        if (l.equals(monitoredLine)) {
+                            isContingency[0] = false;
+                            return;
+                        }
+                    });
+                    return isContingency[0];
+                })
+                .map(l -> new Contingency(l.getId(), new BranchContingency(l.getId())))
+                .collect(Collectors.toList());
+        // This makes a total of 11 contingencies
+        LOGGER.info("Number of contingencies: {}", contingenciesProvider.getContingencies(network).size());
+
+        // For each contingency, create a variant of the network and run a sensitivity analysis.
+        contingenciesProvider.getContingencies(network).forEach(cont -> {
+            // Create a new variant and set it as the WorkingVariant.
+            network.getVariantManager().cloneVariant(VariantManagerConstants.INITIAL_VARIANT_ID,
+                    "contingencyVariant");
+            network.getVariantManager().setWorkingVariant("contingencyVariant");
+            // Apply the contingency on the network in that variant.
+            cont.toTask().modify(network, computationManager);
+
+            // Run the sensitivity computation with respect to the PST tap position on that network.
+            SensitivityComputationResults contingencyResults = sensitivityComputation.run(
+                    twtFactorsProvider, "contingencyVariant",
+                    SensitivityComputationParameters.load()).join();
+
+            // Print the sensitivity values in the terminal.
+            LOGGER.info("Contingency {}:", cont.getId());
+            contingencyResults.getSensitivityValues().forEach(value ->
+                LOGGER.info("Value: {}", value.getValue()));
+
+            // Remove the variant corresponding to the current contingency.
+            network.getVariantManager().removeVariant("contingencyVariant");
+        });
+    }
+
+    private SensitivityTutorial() {
+    }
+}

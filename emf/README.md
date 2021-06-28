18e3417bf25d6dfeb113b192b79f16ac364a4222
Coline Piloquet <coline.piloquet@rte-france.com>
2021-06-28 14:41:12 +0200

    Move tutorial to emf folder and add initial/complete
    Signed-off-by: Coline Piloquet <coline.piloquet@rte-france.com>

---
+++ emf/README.md
@@ -0,0 +1,4 @@
+# Merging View, Balance adjustment and SV export of CGM
+
+In this tutorial, we will learn how to import multiple IGMs, merge them and compute a loadflow on the resulting CGM. Then we will learn how to balance the net positions of the merged areas and export the results.
+For full documentation, please visit [this page](link_to_add).
---
+++ emf/complete/src/main/java/powsybl/tutorials/mergingview/BalancesAdjustmentValidationParameters.java
@@ -0,0 +1,74 @@
+/**
+ * Copyright (c) 2021, RTE (http://www.rte-france.com)
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package powsybl.tutorials.mergingview;
+
+import com.powsybl.commons.config.ModuleConfig;
+import com.powsybl.commons.config.PlatformConfig;
+
+import java.util.*;
+
+/**
+ * @author Miora Ralambotiana <miora.ralambotiana at rte-france.com>
+ */
+public class BalancesAdjustmentValidationParameters {
+
+    private final Map<String, String> igmPaths = new HashMap<>();
+    //private final List<String> excludedXnodes = new ArrayList<>();
+
+    private String dataExchangesPath = null;
+    private String outputDir = null;
+
+    public static BalancesAdjustmentValidationParameters load() {
+        BalancesAdjustmentValidationParameters parameters = new BalancesAdjustmentValidationParameters();
+        PlatformConfig platformConfig = PlatformConfig.defaultConfig();
+
+        ModuleConfig config = platformConfig.getModuleConfig("balances-adjustment-validation-parameters");
+        config.getStringListProperty("igm-paths").forEach(path -> {
+            String[] pathArray = path.split(",");
+            parameters.putIgmPath(pathArray[0].replaceAll("\\s+", ""), pathArray[1].replaceAll("\\s+", ""));
+        });
+        parameters.setDataExchangesPath(config.getStringProperty("data-exchanges-path"));
+        //config.getOptionalStringListProperty("excluded-Xnodes").ifPresent(parameters::setExcludedXnodes);
+        config.getOptionalStringProperty("output-dir").ifPresent(parameters::setOutputDir);
+        return parameters;
+    }
+
+    private void putIgmPath(String name, String path) {
+        igmPaths.put(name, path);
+    }
+
+    public Map<String, String> getIgmPaths() {
+        return Collections.unmodifiableMap(igmPaths);
+    }
+
+    private void setDataExchangesPath(String dataExchangesPath) {
+        this.dataExchangesPath = Objects.requireNonNull(dataExchangesPath);
+    }
+
+    public String getDataExchangesPath() {
+        return dataExchangesPath;
+    }
+
+    //private void setExcludedXnodes(List<String> excludedXnodes) {
+    //    this.excludedXnodes.addAll(Objects.requireNonNull(excludedXnodes));
+    //}
+
+    public Optional<String> getOutputDir() {
+        return Optional.ofNullable(outputDir);
+    }
+
+    private void setOutputDir(String outputDir) {
+        this.outputDir = Objects.requireNonNull(outputDir);
+    }
+
+    //public List<String> getExcludedXnodes() {
+    //    return Collections.unmodifiableList(excludedXnodes);
+    //}
+
+    private BalancesAdjustmentValidationParameters() {
+    }
+}

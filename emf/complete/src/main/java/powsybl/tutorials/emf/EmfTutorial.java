3e54343d1ec25570180bfb6d236460c7bd395023
Coline Piloquet <coline.piloquet@rte-france.com>
2021-07-12 13:35:02 +0200

    add pom in initial and change package name in complete
    Signed-off-by: Coline Piloquet <coline.piloquet@rte-france.com>

--- emf/complete/src/main/java/powsybl/tutorials/emf/EmfTutorial.java
+++ emf/complete/src/main/java/powsybl/tutorials/emf/EmfTutorial.java
@@ -4,7 +4,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-package powsybl.tutorials.mergingview;
+package powsybl.tutorials.emf;
 
 import com.powsybl.action.util.Scalable;
 import com.powsybl.balances_adjustment.balance_computation.*;

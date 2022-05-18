a137a6e476ec84397bb10ebe7d9bf537921b6c8f
Pierre-Marie Manach <pierre-marie.manach@artelys.com>
2022-05-18 17:27:49 +0200

    initialize metrix tutorial
    Signed-off-by: pimanc <pierre-marie.manach@artelys.com>

---
+++ metrix/README.md
@@ -0,0 +1,2 @@
+# Metrix tutorial
+This tutorial aims to show how to perform OPF using Metrix on a small fictive network.
---
+++ metrix/src/main/resources/3A/conf.groovy
@@ -0,0 +1,8 @@
+
+for (l in network.branches){
+branch(l.id){
+baseCaseFlowResults true
+maxThreatFlowResults true
+}
+}
+

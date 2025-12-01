01f431aecae2e59f2675ad3ce45d6591ac6b3a90
Nicolas Rol <nicolas.rol@rte-france.com>
2025-12-01 10:02:40 +0100

    Replace javacsv with FastCSV
    Signed-off-by: Nicolas Rol <nicolas.rol@rte-france.com>

--- csv-importer/src/main/java/com/powsybl/tutorials/csv/import_/CsvLinesImporter.java
+++ csv-importer/src/main/java/com/powsybl/tutorials/csv/import_/CsvLinesImporter.java
@@ -6,17 +6,24 @@
  */
 package com.powsybl.tutorials.csv.import_;
 
-import com.csvreader.CsvReader;
 import com.google.auto.service.AutoService;
 import com.powsybl.commons.datasource.ReadOnlyDataSource;
+import com.powsybl.iidm.network.Bus;
+import com.powsybl.iidm.network.Country;
 import com.powsybl.iidm.network.Importer;
-import com.powsybl.iidm.network.*;
+import com.powsybl.iidm.network.Network;
+import com.powsybl.iidm.network.NetworkFactory;
+import com.powsybl.iidm.network.Substation;
+import com.powsybl.iidm.network.TopologyKind;
+import com.powsybl.iidm.network.VoltageLevel;
+import de.siegmar.fastcsv.reader.CsvReader;
+import de.siegmar.fastcsv.reader.NamedCsvRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.UncheckedIOException;
-import java.nio.charset.StandardCharsets;
 import java.util.Properties;
 
 /**
@@ -53,44 +60,42 @@ public class CsvLinesImporter implements Importer {
     public Network importData(ReadOnlyDataSource dataSource, NetworkFactory networkFactory, Properties parameters) {
         Network network = networkFactory.createNetwork("Network_2Lines_Example", EXTENSION);
         LOGGER.debug("Start import from file {}", dataSource.getBaseName());
-        try {
-            CsvReader reader = new CsvReader(dataSource.newInputStream(null, EXTENSION), StandardCharsets.UTF_8);
-            try {
-                reader.readHeaders();
-                while (reader.readRecord()) {
-                    String id = reader.get("LineId");
-                    LOGGER.info("import lineID {}", id);
-                    Substation s1 = getSubstation(reader.get("SubstationId1"), network, Country.FR);
-                    Substation s2 = getSubstation(reader.get("SubstationId2"), network, Country.BE);
-                    VoltageLevel vl1 = getVoltageLevel(reader.get("VoltageLevelId1"), network, s1, 220, TopologyKind.BUS_BREAKER);
-                    VoltageLevel vl2 = getVoltageLevel(reader.get("VoltageLevelId2"), network, s2, 220, TopologyKind.BUS_BREAKER);
-                    Bus nhv1 = getBus(vl1, reader.get("BusId1"));
-                    Bus nhv2 = getBus(vl2, reader.get("BusId2"));
-                    network.newLine()
-                            .setId(id)
-                            .setVoltageLevel1(vl1.getId())
-                            .setVoltageLevel2(vl2.getId())
-                            .setBus1(nhv1.getId())
-                            .setConnectableBus1(nhv1.getId())
-                            .setBus2(nhv2.getId())
-                            .setConnectableBus2(nhv2.getId())
-                            .setR(Double.valueOf(reader.get("R")))
-                            .setX(Double.valueOf(reader.get("X")))
-                            .setG1(Double.valueOf(reader.get("G1")))
-                            .setB1(Double.valueOf(reader.get("B1")))
-                            .setG2(Double.valueOf(reader.get("G2")))
-                            .setB2(Double.valueOf(reader.get("B2")))
-                            .add();
-                }
-            } finally {
-                reader.close();
-            }
+        try (InputStream inputStream = dataSource.newInputStream(null, EXTENSION);
+             CsvReader<NamedCsvRecord> csvReader = CsvReader.builder()
+                 .fieldSeparator(",")
+                 .quoteCharacter('"')
+                 .ofNamedCsvRecord(inputStream)) {
+            csvReader.forEach(rec -> {
+                String id = rec.getField("LineId");
+                LOGGER.info("import lineID {}", id);
+                Substation s1 = getSubstation(rec.getField("SubstationId1"), network, Country.FR);
+                Substation s2 = getSubstation(rec.getField("SubstationId2"), network, Country.BE);
+                VoltageLevel vl1 = getVoltageLevel(rec.getField("VoltageLevelId1"), network, s1, 220, TopologyKind.BUS_BREAKER);
+                VoltageLevel vl2 = getVoltageLevel(rec.getField("VoltageLevelId2"), network, s2, 220, TopologyKind.BUS_BREAKER);
+                Bus nhv1 = getBus(vl1, rec.getField("BusId1"));
+                Bus nhv2 = getBus(vl2, rec.getField("BusId2"));
+                network.newLine()
+                    .setId(id)
+                    .setVoltageLevel1(vl1.getId())
+                    .setVoltageLevel2(vl2.getId())
+                    .setBus1(nhv1.getId())
+                    .setConnectableBus1(nhv1.getId())
+                    .setBus2(nhv2.getId())
+                    .setConnectableBus2(nhv2.getId())
+                    .setR(Double.parseDouble(rec.getField("R")))
+                    .setX(Double.parseDouble(rec.getField("X")))
+                    .setG1(Double.parseDouble(rec.getField("G1")))
+                    .setB1(Double.parseDouble(rec.getField("B1")))
+                    .setG2(Double.parseDouble(rec.getField("G2")))
+                    .setB2(Double.parseDouble(rec.getField("B2")))
+                    .add();
+            });
             LOGGER.debug("{} import done", EXTENSION);
-            return network;
 
-        } catch (IOException e) {
-            throw new UncheckedIOException(e);
+        } catch (IOException ioException) {
+            throw new UncheckedIOException(ioException);
         }
+        return network;
     }
 
     private static Substation getSubstation(String id, Network network, Country country) {

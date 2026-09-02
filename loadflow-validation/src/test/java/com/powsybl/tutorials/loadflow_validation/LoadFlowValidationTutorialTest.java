66e1791ca4a9b2f15069d5880fb0b8b7b776dd41
Samir Romdhani <samir.romdhani_externe@rte-france.com>
2026-09-02 15:34:10 +0200

    updates
    Signed-off-by: Samir Romdhani <samir.romdhani_externe@rte-france.com>

--- loadflow-validation/src/test/java/com/powsybl/tutorials/loadflow_validation/LoadFlowValidationTutorialTest.java
+++ loadflow-validation/src/test/java/com/powsybl/tutorials/loadflow_validation/LoadFlowValidationTutorialTest.java
@@ -10,10 +10,7 @@ package com.powsybl.tutorials.loadflow_validation;
 
 import com.google.common.jimfs.Configuration;
 import com.google.common.jimfs.Jimfs;
-import com.powsybl.iidm.network.Bus;
-import com.powsybl.iidm.network.Load;
-import com.powsybl.iidm.network.Network;
-import com.powsybl.iidm.network.Terminal;
+import com.powsybl.iidm.network.*;
 import com.powsybl.loadflow.LoadFlow;
 import com.powsybl.loadflow.LoadFlowParameters;
 import com.powsybl.loadflow.validation.ValidationConfig;
@@ -23,17 +20,20 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.nio.file.FileSystem;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Comparator;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class LoadFlowValidationTutorialTest {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(LoadFlowValidationTutorialTest.class);
 
     protected FileSystem fileSystem;
     protected Path tmpDir;
@@ -72,104 +72,112 @@ class LoadFlowValidationTutorialTest {
     @Test
     void testValidationOfBuses() throws IOException {
         Network network = Network.read("network.xiidm", getClass().getResourceAsStream("/network.xiidm"));
-        assertThat(ValidationType.BUSES.check(network, ValidationConfig.load(), tmpDir)).isEqualTo(false);
+        assertThat(ValidationType.BUSES.check(network, ValidationConfig.load(), tmpDir)).isFalse();
         String result = """
                 sim1 BUSES check
                 id;characteristic;value
-                VLGEN_0;incomingP;inv
-                VLGEN_0;incomingQ;inv
+                VLGEN_0;incomingP;NOT_CALCULATED
+                VLGEN_0;incomingQ;NOT_CALCULATED
                 VLGEN_0;loadP;0.00000
                 VLGEN_0;loadQ;0.00000
-                VLHV1_0;incomingP;inv
-                VLHV1_0;incomingQ;inv
+                VLHV1_0;incomingP;NOT_CALCULATED
+                VLHV1_0;incomingQ;NOT_CALCULATED
                 VLHV1_0;loadP;0.00000
                 VLHV1_0;loadQ;0.00000
-                VLHV2_0;incomingP;inv
-                VLHV2_0;incomingQ;inv
+                VLHV2_0;incomingP;NOT_CALCULATED
+                VLHV2_0;incomingQ;NOT_CALCULATED
                 VLHV2_0;loadP;0.00000
                 VLHV2_0;loadQ;0.00000
-                VLLOAD_0;incomingP;inv
-                VLLOAD_0;incomingQ;inv
-                VLLOAD_0;loadP;inv
-                VLLOAD_0;loadQ;inv
+                VLLOAD_0;incomingP;NOT_CALCULATED
+                VLLOAD_0;incomingQ;NOT_CALCULATED
+                VLLOAD_0;loadP;NOT_CALCULATED
+                VLLOAD_0;loadQ;NOT_CALCULATED
                 """;
-        // assertThat(tmpDir.resolve("buses.csv")).content().isEqualTo(result);
+        assertThat(tmpDir.resolve("buses.csv")).content().isEqualTo(result);
+        Generator generator = network.getGenerator("GEN");
+        TwoWindingsTransformer transformer = network.getTwoWindingsTransformer("NGEN_NHV1");
+        LOGGER.info("Validation before LoadFlow");
+        LOGGER.info("GEN P: {} MW", generator.getTerminal().getP());
+        LOGGER.info("Transformer P: {} MW", transformer.getTerminal1().getP());
+        LOGGER.info("SUM : {}", generator.getTerminal().getP() + transformer.getTerminal1().getP());
         // loadflow validation
         LoadFlow.run(network, new LoadFlowParameters());
         // check loadflow validation after loadflow run
-        network.getBusView()
-                .getBusStream()
-                .sorted(Comparator.comparing(Bus::getId))
-                .forEach(bus -> {
-                    System.out.println("bus ID :: " + bus.getId());
-                    System.out.println("    >>>> P :: " + bus.getP());
-                    System.out.println("    >>>> Q :: " + bus.getQ());
-                    double loadP = bus.getLoadStream()
-                            .map(Load::getTerminal).mapToDouble(Terminal::getP).sum();
-                    double loadQ = bus.getLoadStream()
-                            .map(Load::getTerminal).mapToDouble(Terminal::getQ).sum();
-                    System.out.println("    <<<< P :: " + loadP);
-                    System.out.println("    <<<< Q :: " + loadQ);
-                });
-        assertThat(ValidationType.BUSES.check(network, ValidationConfig.load(), tmpDir)).isEqualTo(true);
-        //[main] INFO com.powsybl.loadflow.validation.BusesValidation - Checking buses of network sim1
-        //[main] WARN com.powsybl.loadflow.validation.BusesValidation - BUSES validation error: VLGEN_0 P -1.0630223528096394E-5 0.0
-        //[main] WARN com.powsybl.loadflow.validation.BusesValidation - BUSES validation error: VLHV1_0 P -0.0040426552814096794 0.0
-        //[main] WARN com.powsybl.loadflow.validation.BusesValidation - BUSES validation error: VLHV1_0 Q 7.022229112862988E-5 0.0
-        //[main] WARN com.powsybl.loadflow.validation.BusesValidation - BUSES validation error: VLHV2_0 P 2.3449047148460522E-9 0.0
-        //[main] WARN com.powsybl.loadflow.validation.BusesValidation - BUSES validation error: VLHV2_0 Q -1.1810016076196916E-8 0.0
-        //[main] WARN com.powsybl.loadflow.validation.BusesValidation - BUSES validation error: VLLOAD_0 P -599.999999904206 600.0
-        //[main] WARN com.powsybl.loadflow.validation.BusesValidation - BUSES validation error: VLLOAD_0 Q -199.999999996566 200.0
+        assertThat(ValidationType.BUSES.check(network, ValidationConfig.load(), tmpDir)).isTrue();
+        LOGGER.info("Validation after LoadFlow");
+        LOGGER.info("GEN P: {} MW", generator.getTerminal().getP());
+        LOGGER.info("Transformer P: {} MW", transformer.getTerminal1().getP());
+        LOGGER.info("SUM : {}", generator.getTerminal().getP() + transformer.getTerminal1().getP());
+        assertThat(tmpDir.resolve("buses.csv")).content().isEqualTo("""
+                sim1 BUSES check
+                id;characteristic;value
+                VLGEN_0;incomingP;-1.06302e-05
+                VLGEN_0;incomingQ;0.00000
+                VLGEN_0;loadP;0.00000
+                VLGEN_0;loadQ;0.00000
+                VLHV1_0;incomingP;-0.00404266
+                VLHV1_0;incomingQ;7.02223e-05
+                VLHV1_0;loadP;0.00000
+                VLHV1_0;loadQ;0.00000
+                VLHV2_0;incomingP;2.34490e-09
+                VLHV2_0;incomingQ;-1.18100e-08
+                VLHV2_0;loadP;0.00000
+                VLHV2_0;loadQ;0.00000
+                VLLOAD_0;incomingP;-600.000
+                VLLOAD_0;incomingQ;-200.000
+                VLLOAD_0;loadP;600.000
+                VLLOAD_0;loadQ;200.000
+                """);
     }
 
     @Test
     void testValidationOfFlows() throws IOException {
         Network network = Network.read("network.xiidm", getClass().getResourceAsStream("/network.xiidm"));
-        assertThat(ValidationType.FLOWS.check(network, ValidationConfig.load(), tmpDir)).isEqualTo(false);
+        assertThat(ValidationType.FLOWS.check(network, ValidationConfig.load(), tmpDir)).isFalse();
         // loadflow validation
         LoadFlow.run(network, new LoadFlowParameters());
         // check loadflow validation after loadflow run
-        assertThat(ValidationType.FLOWS.check(network, ValidationConfig.load(), tmpDir)).isEqualTo(true);
+        assertThat(ValidationType.FLOWS.check(network, ValidationConfig.load(), tmpDir)).isTrue();
     }
 
     @Test
     void testValidationOfGenerators() throws IOException {
         Network network = Network.read("network.xiidm", getClass().getResourceAsStream("/network.xiidm"));
-        assertThat(ValidationType.GENERATORS.check(network, ValidationConfig.load(), tmpDir)).isEqualTo(false);
+        assertThat(ValidationType.GENERATORS.check(network, ValidationConfig.load(), tmpDir)).isFalse();
         // loadflow validation
         LoadFlow.run(network, new LoadFlowParameters());
         // check loadflow validation after loadflow run
-        assertThat(ValidationType.GENERATORS.check(network, ValidationConfig.load(), tmpDir)).isEqualTo(true);
+        assertThat(ValidationType.GENERATORS.check(network, ValidationConfig.load(), tmpDir)).isTrue();
     }
 
     @Test
     void testValidationOfSvc() throws IOException {
         Network network = Network.read("network.xiidm", getClass().getResourceAsStream("/network.xiidm"));
-        assertThat(ValidationType.SVCS.check(network, ValidationConfig.load(), tmpDir)).isEqualTo(true);
+        assertThat(ValidationType.SVCS.check(network, ValidationConfig.load(), tmpDir)).isTrue();
         // loadflow validation
         LoadFlow.run(network, new LoadFlowParameters());
         // check loadflow validation after loadflow run
-        assertThat(ValidationType.SVCS.check(network, ValidationConfig.load(), tmpDir)).isEqualTo(true);
+        assertThat(ValidationType.SVCS.check(network, ValidationConfig.load(), tmpDir)).isTrue();
     }
 
     @Test
     void testValidationOfTWT() throws IOException {
         Network network = Network.read("network.xiidm", getClass().getResourceAsStream("/network.xiidm"));
-        assertThat(ValidationType.TWTS.check(network, ValidationConfig.load(), tmpDir)).isEqualTo(false);
+        assertThat(ValidationType.TWTS.check(network, ValidationConfig.load(), tmpDir)).isFalse();
         // loadflow validation
         LoadFlow.run(network, new LoadFlowParameters());
         // check loadflow validation after loadflow run
-        assertThat(ValidationType.TWTS.check(network, ValidationConfig.load(), tmpDir)).isEqualTo(true);
+        assertThat(ValidationType.TWTS.check(network, ValidationConfig.load(), tmpDir)).isTrue();
     }
 
     @Test
     void testValidationOfTWT3W() throws IOException {
         Network network = Network.read("network.xiidm", getClass().getResourceAsStream("/network.xiidm"));
-        assertThat(ValidationType.TWTS3W.check(network, ValidationConfig.load(), tmpDir)).isEqualTo(true);
+        assertThat(ValidationType.TWTS3W.check(network, ValidationConfig.load(), tmpDir)).isTrue();
         // loadflow validation
         LoadFlow.run(network, new LoadFlowParameters());
         // check loadflow validation after loadflow run
-        assertThat(ValidationType.TWTS3W.check(network, ValidationConfig.load(), tmpDir)).isEqualTo(true);
+        assertThat(ValidationType.TWTS3W.check(network, ValidationConfig.load(), tmpDir)).isTrue();
     }
 
 }
